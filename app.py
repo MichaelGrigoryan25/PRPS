021b6724690d2bb907a89ada5a0b005db8a77844
Michael <michael.grigoryan25@gmail.com>
2020-07-20 17:00:36 +0400

    Minor changes

--- app.py
+++ app.py
@@ -2,26 +2,32 @@ import random
 
 OptionsList = ['rock', 'paper', 'scissors']
 CPUOption = random.choice(OptionsList)
-line = "_______________________"
 
+line = "_______________________"
+ErrorMessage = "Error. Please start again"
 UserInput = input("Choose an Option: " + str(OptionsList))
 
-if UserInput == "rock" and CPUOption == "paper":
-    print(line)
-    print(CPUOption + "\nYou Lost")
-elif UserInput == "paper" and CPUOption == "scissors":
-    print(line)
-    print(CPUOption + "\nYou Lost")
-elif UserInput == "scissors" and CPUOption == "rock":
-    print(line)
-    print(CPUOption + "\nYou Lost")
-elif UserInput == CPUOption:
-    print(line)
-    print("DRAW!")
-elif UserInput == "":
-    print("You didn't enter anything")
-elif UserInput != OptionsList:
-    print("Error")
-else:
-    print(line + "\nCPU chose: " + CPUOption)
-    print("You won!")
+def start():
+    if UserInput == "rock" and CPUOption == "paper":
+        print(line)
+        print(CPUOption + "\nYou Lost")
+    elif UserInput == "paper" and CPUOption == "scissors":
+        print(line)
+        print(CPUOption + "\nYou Lost")
+    elif UserInput == "scissors" and CPUOption == "rock":
+        print(line)
+        print(CPUOption + "\nYou Lost")
+    elif UserInput == CPUOption:
+        print(line)
+        print("DRAW!")
+    elif UserInput == "":
+        print(ErrorMessage)
+        start()
+    elif UserInput != OptionsList:
+        print(ErrorMessage)
+        start()
+    else:
+        print(line + "\nCPU chose: " + CPUOption)
+        print("You won!")
+
+start()
