86a02344d9790c81499385f0303ec5dc362da7f2
Michael Grigoryan <56165400+MichaelGrigoryan25@users.noreply.github.com>
2020-07-20 15:51:22 +0400

    Improved formatting

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import random
 
 OptionsList = ['rock', 'paper', 'scissors']
-CPUOption = random.choice(ChoiceList)
+CPUOption = random.choice(OptionsList)
+line = "_______________________"
 
-UserInput = input("Choose an Option: " + OptionsList)
+UserInput = input("Choose an Option: " + str(OptionsList))
 
 if UserInput == "rock" and CPUOption == "paper":
-    print(CPUOption + "You Lost")
+    print(line)
+    print(CPUOption + "\nYou Lost")
 elif UserInput == "paper" and CPUOption == "scissors":
-    print(CPUOption + "You Lost")
+    print(line)
+    print(CPUOption + "\nYou Lost")
 elif UserInput == "scissors" and CPUOption == "rock":
-    print(CPUOption + "You Lost")
+    print(line)
+    print(CPUOption + "\nYou Lost")
 elif UserInput == CPUOption:
+    print(line)
     print("DRAW!")
 else:
-    print("CPU chose: " + CPUOption + /n"You Won!")
+    print(line + "\nCPU chose: " + CPUOption)
+    print("You won!")
