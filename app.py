9f071bb0c4e1e95918a42b1fa5bc3641578cb78f
Michael <michael.grigoryan25@gmail.com>
2020-07-20 19:04:31 +0400

    Minor error bug fix

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ import random
 
 OptionsList = ['rock', 'paper', 'scissors']
 CPUOption = random.choice(OptionsList)
-
 line = "_______________________"
 ErrorMessage = "Error. Please start again"
-UserInput = input("Choose an Option: " + str(OptionsList))
+UserInput = str(input("Choose an Option -- " + str(OptionsList) + ": "))
 
 def start():
     if UserInput == "rock" and CPUOption == "paper":
         print(line)
         print(CPUOption + "\nYou Lost")
+    elif UserInput == '' and OptionsList != UserInput:
+        print(ErrorMessage)
     elif UserInput == "paper" and CPUOption == "scissors":
         print(line)
         print(CPUOption + "\nYou Lost")
@@ -20,12 +21,6 @@ def start():
     elif UserInput == CPUOption:
         print(line)
         print("DRAW!")
-    elif UserInput == "":
-        print(ErrorMessage)
-        start()
-    elif UserInput != OptionsList:
-        print(ErrorMessage)
-        start()
     else:
         print(line + "\nCPU chose: " + CPUOption)
         print("You won!")
